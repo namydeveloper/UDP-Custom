81bb333c2fb4029f2c3dfba3149130758802efda
namydeveloper <122620672+namydeveloper@users.noreply.github.com>
2024-02-25 22:20:08 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -51,6 +51,18 @@ WantedBy=default.target
 EOF
 fi
 
+setup_ssl() {
+	echo "Installing ssl"
+
+	openssl genrsa -out /etc/hysteria/hysteria.ca.key 2048
+
+	openssl req -new -x509 -days 3650 -key /etc/hysteria/hysteria.ca.key -subj "/C=CN/ST=GD/L=SZ/O=Hysteria, Inc./CN=Hysteria Root CA" -out /etc/hysteria/hysteria.ca.crt
+
+	openssl req -newkey rsa:2048 -nodes -keyout /etc/hysteria/hysteria.server.key -subj "/C=CN/ST=GD/L=SZ/O=Hysteria, Inc./CN=$DOMAIN" -out /etc/hysteria/hysteria.server.csr
+
+	openssl x509 -req -extfile <(printf "subjectAltName=DNS:$DOMAIN,DNS:$DOMAIN") -days 3650 -in /etc/hysteria/hysteria.server.csr -CA /etc/hysteria/hysteria.ca.crt -CAkey /etc/hysteria/hysteria.ca.key -CAcreateserial -out /etc/hysteria/hysteria.server.crt	
+ }
+
 echo start service udp-custom
 systemctl start udp-custom &>/dev/null
 
